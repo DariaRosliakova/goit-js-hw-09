a1bfac601dff2361ca9ce124fa687f241618a953
DariaRosliakova <107692009+DariaRosliakova@users.noreply.github.com>
2022-11-21 17:06:04 +0000

    Deploying to gh-pages from @ DariaRosliakova/goit-js-hw-09@c0912aadeeb59d9f4c1db8dd80df2b0228e98fd1 🚀

---
+++ 01-color-switcher.3dda1b1a.js
@@ -0,0 +1,2 @@
+function e(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}const t=document.querySelector("body"),n=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");n.addEventListener("click",(function(){n.disabled=!0,o.disabled=!1,t.style.backgroundColor=e(),newBackground=setInterval((()=>{t.style.backgroundColor=e()}),1e3)})),o.addEventListener("click",(function(){o.disabled=!0,n.disabled=!1,clearInterval(newBackground)}));
+//# sourceMappingURL=01-color-switcher.3dda1b1a.js.map
